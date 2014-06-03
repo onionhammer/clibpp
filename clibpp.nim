f98cbbf0cadc02e071d7dc79178b01a5f7917772
Billingsly Wetherfordshire <phowl.mouth@gmail.com>
2014-06-02 21:31:23 -0500

    able to compile the test program without the makefile

--- clibpp.nim
+++ clibpp.nim
@@ -134,7 +134,7 @@ macro class*(className, opts: expr, body: stmt): stmt {.immediate.} =
 
 when isMainModule:
 
-    {.link: "test.o".}
+    {.compile: "test.cpp".}#link: "test.o".}
 
     when false:
         # Traditional wrapper

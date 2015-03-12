25d47ce1f9e954b2c9e69577b7f3c4d247bbc6bd
fowlmouth <phowl.mouth@gmail.com>
2015-03-12 11:56:53 -0500

    added testing coverage of inheritance

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include "test.hpp"
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 int pp::test::notherName = 120;
 
@@ -12,3 +13,5 @@ int pp::test::multiply(int value, int by) {
 	fieldName = 240;
 	return value * by;
 }
+
+int pp::test_sub::childf() { return 42; }
